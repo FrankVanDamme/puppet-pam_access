fd767ddd02499a388d603a9fca8477e45b46848c
Frank Van Damme <frank.van.damme@vub.be>
2021-05-19 16:01:07 +0200

    finally made modifying the pam files technically idempotent

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,40 +36,50 @@ class pam_access (
    }
 
     case $::operatingsystem { 
-	'RedHat', 'CentOS': { 
-	    $pam_acc_enable="authconfig --enablelocauthorize --enablepamaccess --enablemkhomedir --update" 
-	    exec { "authconfig-access":
-	      command => $pam_acc_enable, 
-	      unless  => "grep '^account.*required.*pam_access.so' \
-		  /etc/pam.d/system-auth 2>/dev/null",
-	      path    => "/usr/bin:/usr/sbin:/bin",
-	      require => File["/etc/security/access.conf"],
-	    }
-	}
-	'debian': { 
-	    $homedir_line = "session	optional	pam_mkhomedir.so umask=${homedir_umask}"
-	    $pam_acc_enable = "grep '^[^#].*pam_access' login >/dev/null && grep '^[^#].*pam_access' sshd  >/dev/null     || \
-		sed -i -e 's/^# *\(.*pam_access.*\)/\1/' /etc/pam.d/sshd /etc/pam.d/login"
-	    $enable_mkhomedir = "sed -i -e '/.*pam_mkhomedir.*/d' /etc/pam.d/common-session \
-		&& echo '$homedir_line' >> /etc/pam.d/common-session"
-	    $enable_umask = "grep '^[^#].*pam_umask' /etc/pam.d/common-session >/dev/null   ||   \
-		echo 'session	optional	pam_umask.so'  >> /etc/pam.d/common-session"
+        'RedHat', 'CentOS': { 
+            $pam_acc_enable="authconfig --enablelocauthorize --enablepamaccess --enablemkhomedir --update" 
+            exec { "authconfig-access":
+              command => $pam_acc_enable, 
+              unless  => "grep '^account.*required.*pam_access.so' \
+        	  /etc/pam.d/system-auth 2>/dev/null",
+              path    => "/usr/bin:/usr/sbin:/bin",
+              require => File["/etc/security/access.conf"],
+            }
+        }
+        'debian': { 
+            # pam files: login + sshd
+            $pam_acc_enable = "sed -i -e 's/^# *\(.*pam_access.*\)/\1/' /etc/pam.d/sshd /etc/pam.d/login"
+            $pam_acc_enable_unless = "grep '^[^#].*pam_access' login >/dev/null && grep '^[^#].*pam_access' sshd  >/dev/null"
 
-	    exec { "authconfig-access":
-	      command => $pam_acc_enable, 
-	      path    => "/usr/bin:/usr/sbin:/bin",
-	      require => File["/etc/security/access.conf"],
-	    }
-	    exec { "enable_mkhomedir":
-		command  => $enable_mkhomedir,
-		path     => "/usr/bin:/usr/sbin:/bin",
-		unless   => "grep '^$homedir_line$' /etc/pam.d/common-session >/tmp/null",
-	    }
-	    exec { "enable_umask":
-		command => $enable_umask,
-		path    => "/usr/bin:/usr/sbin:/bin",
-	    }
-	} # debian
+            # pam files: common-session
+            $homedir_line = "session	optional	pam_mkhomedir.so umask=${homedir_umask}"
+            $enable_mkhomedir = "sed -i -e '/.*pam_mkhomedir.*/d' /etc/pam.d/common-session \
+                && echo '$homedir_line' >> /etc/pam.d/common-session"
+            $enable_mkhomedir_unless = "grep '^$homedir_line$' /etc/pam.d/common-session >/tmp/null"
+
+            # pam files: common-session
+            $enable_umask = "echo 'session	optional	pam_umask.so'  >> /etc/pam.d/common-session"
+            $enable_umask_unless = "grep '^[^#].*pam_umask' /etc/pam.d/common-session >/dev/null"
+
+            exec { "authconfig-access":
+                command => $pam_acc_enable,
+                unless  => $pam_acc_enable_unless,
+                path    => "/usr/bin:/usr/sbin:/bin",
+                require => File["/etc/security/access.conf"],
+            }
+
+            exec { "enable_mkhomedir":
+                command  => $enable_mkhomedir,
+                unless   => $enable_mkhomedir_unless,
+                path     => "/usr/bin:/usr/sbin:/bin",
+            }
+
+            exec { "enable_umask":
+                command => $enable_umask,
+                unless  => $enable_umask_unless,
+                path    => "/usr/bin:/usr/sbin:/bin",
+            }
+        } # debian
     } # case 
 } # class
 
