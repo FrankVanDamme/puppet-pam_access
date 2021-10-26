434a5ce27aa160587a5da6219421989f49715bf9
Frank Van Damme <frank.van.damme@vub.be>
2021-10-26 13:41:27 +0200

    Added option to enable or disable pam_access in pam. Raise error for Red Hat from version 8 which has no authconfig command anymore

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,7 +19,8 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class pam_access (
-    $homedir_umask = '0022'
+    $homedir_umask = '0022',
+    $access_control_enable = true,
 ){
    # place groups or users in the below arrays.
    # ex: $group = [sudo, foo, bar]
@@ -35,21 +36,37 @@ class pam_access (
          content => template("pam_access/etc/security/access.conf.erb"),
    }
 
-    case $::operatingsystem { 
-        'RedHat', 'CentOS': { 
-            $pam_acc_enable="authconfig --enablelocauthorize --enablepamaccess --enablemkhomedir --update" 
-            exec { "authconfig-access":
-              command => $pam_acc_enable, 
-              unless  => "grep '^account.*required.*pam_access.so' \
-        	  /etc/pam.d/system-auth 2>/dev/null",
-              path    => "/usr/bin:/usr/sbin:/bin",
-              require => File["/etc/security/access.conf"],
+    case $::operatingsystem {
+        'RedHat', 'CentOS': {
+            if ( versioncmp($::facts['os']['release']['major'], '7')  > 0 ){
+                fail ("Red Hat 8 and above not supported!")
+            }
+            $pam_acc_enable="authconfig --enablelocauthorize --enablepamaccess --enablemkhomedir --update"
+            $pam_acc_disable="authconfig --enablelocauthorize --disablepamaccess --enablemkhomedir --update"
+
+            if ( $access_control_enable == true ){
+                exec { "authconfig-access":
+                    command => $pam_acc_enable,
+                    unless  => "grep '^account.*required.*pam_access.so' \
+                    /etc/pam.d/system-auth 2>/dev/null",
+                    path    => "/usr/bin:/usr/sbin:/bin",
+                    require => File["/etc/security/access.conf"],
+                }
+            } else {
+                exec { "authconfig-access":
+                    command => $pam_acc_disable,
+                    onlyif  => "grep '^account.*required.*pam_access.so' \
+                    /etc/pam.d/system-auth 2>/dev/null",
+                    path    => "/usr/bin:/usr/sbin:/bin",
+                    require => File["/etc/security/access.conf"],
+                }
             }
         }
-        'debian': { 
+        'debian': {
             # pam files: login + sshd
             # enable pam_access module
             $pam_acc_enable = "sed -i -e 's/^# *\(.*pam_access.*\)/\1/' /etc/pam.d/sshd /etc/pam.d/login"
+            $pam_acc_disable = "sed -i -e 's/^ *\(.*pam_access.*\)/#\1/' /etc/pam.d/sshd /etc/pam.d/login"
             $pam_acc_enable_unless = "grep '^[^#].*pam_access' login >/dev/null && grep '^[^#].*pam_access' sshd  >/dev/null"
 
             # pam files: common-session
@@ -64,11 +81,20 @@ class pam_access (
             $enable_umask = "echo 'session	optional	pam_umask.so'  >> /etc/pam.d/common-session"
             $enable_umask_unless = "grep '^[^#].*pam_umask' /etc/pam.d/common-session >/dev/null"
 
-            exec { "authconfig-access":
-                command => $pam_acc_enable,
-                unless  => $pam_acc_enable_unless,
-                path    => "/usr/bin:/usr/sbin:/bin",
-                require => File["/etc/security/access.conf"],
+            if ( $access_control_enable == true ){
+                exec { "authconfig-access":
+                    command => $pam_acc_enable,
+                    unless  => $pam_acc_enable_unless,
+                    path    => "/usr/bin:/usr/sbin:/bin",
+                    require => File["/etc/security/access.conf"],
+                }
+            } else {
+                exec { "authconfig-access":
+                    command => $pam_acc_disable,
+                    onlyif  => $pam_acc_enable_unless,
+                    path    => "/usr/bin:/usr/sbin:/bin",
+                    require => File["/etc/security/access.conf"],
+                }
             }
 
             exec { "enable_mkhomedir":
