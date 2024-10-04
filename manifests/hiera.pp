2fa358f3e8a2f8efb9923abb095c233fa9a80523
Frank Van Damme <frank.van.damme@vub.be>
2024-10-04 16:23:36 +0200

    remove legacy fact

--- manifests/hiera.pp
+++ manifests/hiera.pp
@@ -1,3 +1,5 @@
+# Haal users en groepen uit hiera om pam_access config file
+# /etc/security/access.conf te maken
 class pam_access::hiera inherits pam_access {
     $u_bl=hiera_array( "users_blacklist", [] )
     $u_wl=hiera_array( "users_whitelist", [] )
@@ -8,14 +10,14 @@ class pam_access::hiera inherits pam_access {
     # Could actually be done by using the 4 hiera variables
     # directly, and adding them to the template in the order
     # "blacklist, whitelist". pam_access will use the first match. 
-    $g__wl = union( [ $hostname ] , $g_wl )
-    
+    $g__wl = union( [ $facts[networking][hostname] ] , $g_wl )
+
     $users = $u_wl - $u_bl
     $group = $g__wl - $g_bl
 
     $other=hiera_hash( "pam_access", {} )
 
     File["/etc/security/access.conf"] {
-	content => template("${module_name}/etc/security/access.conf.erb"),
+        content => template("${module_name}/etc/security/access.conf.erb"),
     }
 }
